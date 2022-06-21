2b445fa74e7bf9403f991ba11ab32920d97b5762
stip777 <95652526+stip777@users.noreply.github.com>
2022-06-21 13:55:39 +0800

    create index.html

---
+++ src/main/java/com/social/socialMedia/AppController.java
@@ -0,0 +1,13 @@
+package com.social.socialMedia;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	@GetMapping("")
+	public String viewHomePage() {
+		return "index";
+	}
+}
