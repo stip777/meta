46f30d913f33850bee84252a689f0054a3a4fdd7
stip777 <95652526+stip777@users.noreply.github.com>
2022-06-21 13:12:03 +0800

    first commit

---
+++ src/test/java/com/social/socialMedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.socialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
