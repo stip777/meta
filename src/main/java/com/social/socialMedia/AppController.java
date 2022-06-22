ebc4427ceb21454dcae258a684441e7c385a8d64
stip777 <95652526+stip777@users.noreply.github.com>
2022-06-22 14:12:05 +0800

    create registration.html and main.html

--- src/main/java/com/social/socialMedia/AppController.java
+++ src/main/java/com/social/socialMedia/AppController.java
@@ -2,12 +2,28 @@ package com.social.socialMedia;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
 
 	@GetMapping("")
-	public String viewHomePage() {
+	public String landingPage() {
 		return "index";
+	}
+	
+	@RequestMapping("/registration.html")
+	public String registrationPage() {
+	    return "registration";
+	}
+	
+	@RequestMapping("/index.html")
+	public String homePage() {
+		return "index";
+	}
+	
+	@RequestMapping("/main.html")
+	public String mainPage() {
+		return "main";
 	}
 }
